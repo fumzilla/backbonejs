63bf344caaf63351f1c6041759840cf356602e6b
fumzilla <fumi3r@netcourrier.com>
2014-03-18 15:14:21 +0100

    Chap5 Starting Library Book

---
+++ booksite/js/models/book.js
@@ -0,0 +1,14 @@
+var app = app || {}:
+
+// BOOK Model
+// ---------------
+
+app.Book = Backbone.Model.extend({
+  defaults: {
+    coverImage  : 'img/placeholder.png',
+    title       : 'No title',
+    author      : 'Unknow',
+    releaseDate : 'Unknow',
+    keywords    : 'None'
+  }
+});
